f7575d0dd5389eb5d9debe138022bfed32afa484
dave griffiths <dave@fo.am>
2016-06-14 16:23:34 +0100

    sensor tweak

--- sensors/therm.py
+++ sensors/therm.py
@@ -2,24 +2,27 @@ import os
 import glob
 import time
 import osc
- 
+
+# code modified from: http://www.modmypi.com/blog/ds18b20-one-wire-digital-temperature-sensor-and-the-raspberry-pi
+
+# load the OS kernel modules for the sensor
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
- 
+
+# the device file for the sensor
 base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
- 
+
 def read_temp_raw():
     f = open(device_file, 'r')
     lines = f.readlines()
     f.close()
     return lines
- 
+
 def read_temp():
     lines = read_temp_raw()
     while lines[0].strip()[-3:] != 'YES':
-       # time.sleep(0.2)
         lines = read_temp_raw()
     equals_pos = lines[1].find('t=')
     if equals_pos != -1:
@@ -28,13 +31,15 @@ def read_temp():
         # temp_f = temp_c * 9.0 / 5.0 + 32.0
         return temp_c
 
-old_temp = 0	
+old_temp = 0
 
 while True:
 	temp_c = read_temp()
 	print(temp_c)
 	print(temp_c-old_temp)
+        # send the current temperature out over osc
 	osc.Message("/temp",[temp_c]).sendlocal(8889)
+        # sends the difference from the last recorded over osc
 	osc.Message("/tempdiff",[temp_c-old_temp]).sendlocal(8889)
 	old_temp = temp_c
 	time.sleep(0.1)
